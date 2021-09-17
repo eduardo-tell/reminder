71c8b3b62118ade6c6a9baafa13b9bf13046630e
Eduardo Tell <eduardo-tell@hotmail.com>
2021-09-16 23:02:29 -0300

    Adicionando MUI Material

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,22 +1,75 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import "./style.css"
+// import "./style.css"
 
-class CardNota extends Component {
-    constructor(props) {
-        super()
-    }
+// class CardNota extends Component {
+//     constructor(props) {
+//         super()
+//     }
 
-    render() {
-        return (
-            <section className="card-nota">
-                <header>
-                    <h3>{this.props.titulo}</h3>
-                </header>
-                <p>{this.props.text}</p>
-            </section>
-        );
-    }
+//     render() {
+//         return (
+//             <section className="card-nota">
+//                 <header>
+//                     <h3>{this.props.titulo}</h3>
+//                 </header>
+//                 <p>{this.props.text}</p>
+//                 <button> <img src="" alt="" /> </button>
+//             </section>
+//         );
+//     }
+// }
+
+// export default CardNota;
+
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        be{bull}nev{bull}o{bull}lent
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
+class CardNota Component {
+  return (
+    <Box sx={{ minWidth: 275 }}>
+      <Card variant="outlined">{card}</Card>
+    </Box>
+  );
 }
 
 export default CardNota;
