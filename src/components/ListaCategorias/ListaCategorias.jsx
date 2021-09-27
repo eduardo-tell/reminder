746e108f357f77c884aa1fcd7e415bf84d8e3924
Eduardo Tell <eduardo-tell@hotmail.com>
2021-09-26 23:51:43 -0300

    Melhorando o componente de listagem de categorias

--- src/components/ListaCategorias/ListaCategorias.jsx
+++ src/components/ListaCategorias/ListaCategorias.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import TextField from '@mui/material/TextField';
 
 class ListaCategorias extends Component {
 
+    _eventoInput(e) {
+        console.log(e)
+        if (e.key == "Enter") {
+            console.log("Categoria")
+        }
+    }
+
     render() {
         return (
-            "Ola"
+            <form>
+                <ul>
+                    <li> <span> Item </span> </li>
+                </ul>
+                <TextField type="text" onKeyUp={this._eventoInput.bind(this)} id="outlined-basic" label="Pesquisar" variant="outlined" />
+            </form>
         );
     }
 }
